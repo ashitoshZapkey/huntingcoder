688faa2205261341ea29cdeb2647410226feaa54
Ashitosh Dev <ashitosh@zapkey.com>
2023-05-12 11:26:44 +0530

    Get data from file

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -3,8 +3,14 @@ import * as fs from 'fs';
 // Valid entry : http://localhost:3000/api/getBlogs?slug=how-to-learn-php
 // Invalid entry : http://localhost:3000/api/getBlogs?slug="test"
 
-export default function handler(req,res){
-    fs.readdir(`blogdata`,(err,data) =>{
-            res.status(200).json(data);
-    });
+export default async function handler(req,res){
+    let data = await fs.promises.readdir('blogdata');
+    let myfile;
+    let allblogs = [];
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        myfile = await fs.promises.readFile(('blogdata/' + element),'utf-8')
+        allblogs.push(JSON.parse(myfile));
+    }
+    res.status(200).json(allblogs)
 }
